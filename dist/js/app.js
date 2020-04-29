0118554db567ffa411b0a40858fad5e886fa6519
mon <monmon.plaza@gmail.com>
2020-04-29 16:16:15 +0800

    for deployment

---
+++ dist/js/app.js
@@ -0,0 +1,11 @@
+var slider = tns({
+    container: '#slider',
+    items: 1,
+    nav: true,
+    slideBy: 'page',
+    autoplay: true,
+    controls: false,
+    autoplayButtonOutput: false,
+    mouseDrag:true
+    
+  });
